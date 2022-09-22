800654395334d28a783eb56d597b48e3c93e0c27
Soonoh <soonoh.jung@gmail.com>
2022-09-22 16:22:52 +0900

    Apply example of react-infinite-scroller component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import InfiniteScroll from "react-infinite-scroller";
+import {useState} from "react";
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
 
 export default function Home() {
+
+  const[items, setItems] = useState([]);
+  const[hasMore, setHasMore] = useState(true);
+  const[page, setPage] = useState(0);
+  const loadFunc = (page) => {
+    delay(1000).then(() => {
+      console.log("loaded data with params: page=", page)
+      setItems([...items, ...['a', 'b', 'c', 'd', 'e']])
+      setPage(page)
+      setHasMore(page <= 10)
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -21,34 +40,32 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+        <div style={{height: '100%', overflow: 'auto'}}>
+          <InfiniteScroll
+              pageStart={0}
+              loadMore={loadFunc}
+              hasMore={hasMore}
+              threshold={10}
+              loader={<div className="loader" key={0}>Loading ...</div>}
+              useWindow={true}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <div className={styles.grid}>
+            {items.map((item, index) => (
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <a
+                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    key={index}
+                >
+                  <h2>Deploy {item}&rarr;</h2>
+                  <p>
+                    Instantly deploy your Next.js site to a public URL with Vercel.
+                  </p>
+                </a>
+            ))}
+            </div>
+          </InfiniteScroll>
+
         </div>
       </main>
 
